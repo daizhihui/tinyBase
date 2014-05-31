74ec1af5fc38c1304c0e51b5cbe61db6f28cf694
fanxiao001 <fanxiaoseu@gmail.com>
2014-05-31 11:58:08 +0200

    Update ix_internal.h

--- src/ix_internal.h
+++ src/ix_internal.h
@@ -1,8 +1,20 @@
+//
+// File:        ix_internal.h
+// Description: Declarations internal to the IX component
+// Author:      Hyunjung Park (hyunjung@stanford.edu)
+//
+
 #ifndef IX_INTERNAL_H
 #define IX_INTERNAL_H
 
 #include <stdlib.h>
 #include <string.h>
 #include "ix.h"
+
+//
+// Constants and defines
+//
+const int IX_HEADER_PAGE_NUM = 0;
+
 
 #endif // IX_INTERNAL_H
