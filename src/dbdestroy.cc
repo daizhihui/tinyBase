c29e25964c81523d5d4e14b5568ebec720e8a535
fanxiao001 <fanxiaoseu@gmail.com>
2014-06-10 20:07:00 +0200

    Update dbdestroy.cc

--- src/dbdestroy.cc
+++ src/dbdestroy.cc
@@ -1,1 +1,44 @@
 // dbdestroy.cc
+//
+// Author: xiao FAN
+//
+// This shell is provided for the student.
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+#include "rm.h"
+#include "sm.h"
+#include "redbase.h"
+
+using namespace std;
+
+//
+// main
+//
+main(int argc, char *argv[])
+{
+    char *dbname;
+    char command[255] = "rm -r "; //remove all the file in the directory
+    RC rc;
+    
+    // Look for 2 arguments. The first is always the name of the program
+    // that was executed, and the second should be the name of the
+    // database.
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " dbname \n";
+        exit(1);
+    }
+    
+    // The database name is the second argument
+    dbname = argv[1];
+    
+    // Create a subdirectory for the database
+    system (strcat(command,dbname));
+    
+    
+    // Fair amount to be filled in here!!
+    
+    return(0);
+}
