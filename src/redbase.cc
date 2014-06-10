c9ba125c395b1e6870cfaf3c709cbbab55f32005
fanxiao001 <fanxiaoseu@gmail.com>
2014-06-10 20:25:40 +0200

    Update redbase.cc

--- src/redbase.cc
+++ src/redbase.cc
@@ -37,5 +37,21 @@ main(int argc, char *argv[])
     //
     // *********************************
 
+// initialize RedBase components
+    PF_Manager pfm;
+    RM_Manager rmm(pfm);
+    IX_Manager ixm(pfm);
+    SM_Manager smm(ixm, rmm);
+    QL_Manager qlm(smm, ixm, rmm);
+    // open the database
+    if ((rc = smm.OpenDb(dbname))){
+        // call the parser
+        RBparse(pfm, smm, qlm);
+        
+        
+    }
+    
+    // close the database
+    if ((rc = smm.CloseDb()))
     cout << "Bye.\n";
 }
